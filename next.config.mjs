53cdbf05af748809e52fc9e9c7823ae926b18068
Jonas Mattes <github@jmattes.de>
2024-07-23 18:42:39 +0200

    slim down config

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
-import path from "path";
 
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      react: path.resolve("./node_modules/react"),
-      "react-dom": path.resolve("./node_modules/react-dom"),
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
